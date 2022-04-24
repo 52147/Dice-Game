78af26dcb466f60c94146f1974369902f9174aac
Han <79159894+52147@users.noreply.github.com>
2022-04-24 14:58:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+// var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+// var randomImageSource = "images/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0];
+// var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+// image1.setAttribute("src", randomImageSource);
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+// document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 // if(randomNumber1 > randomNumber2){
 //     document.querySelector("h1").innerHTML = "🎲Play 1 Wins!🎲";
